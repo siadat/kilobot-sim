5a37632609a3a24d474d65bcd5bffc46f5753dfd
Sina Siadat <siadat@gmail.com>
2019-10-31 14:31:36 +0330

    simplify the random experiment

--- experiments/random.js
+++ experiments/random.js
@@ -1,14 +1,28 @@
 class RobotRandom extends Kilobot {
   setup() {
-    this.counter = 0;
   }
 
   loop() {
-    this.counter++;
+    let randBetween0to2 = Math.floor(3 * this.rand_soft()/256);
 
-    let left = this.rand_soft()/255.0 > 0.5 ? this.kilo_turn_left : 0;
-    let right = this.rand_soft()/255.0 > 0.5 ? this.kilo_turn_right : 0;
-    this.set_motors(left, right);
+    switch(randBetween0to2) {
+      case 0:
+        this.set_motors(this.kilo_turn_left, 0);
+        break;
+      case 1:
+        this.set_motors(0, this.kilo_turn_right);
+        break;
+      case 2:
+        this.set_motors(this.kilo_straight_left, this.kilo_straight_right);
+        break;
+    }
+  }
+
+  message_rx(message, distance) {
+  }
+
+  message_tx() {
+    return null;
   }
 }
 
@@ -22,25 +36,13 @@ window.ExperimentRandom = class {
   }
 
   createRobots(newRobot) {
-    for(let i = 0; i < 10; i++) {
-      newRobot(
-        {
-          x: 20 * RADIUS * this.rand_soft()/255.0,
-          y: 20 * RADIUS * this.rand_soft()/255.0,
-        },
-        0,
-        new RobotRandom(),
-      );
+    for(let i = 0; i < 1; i++) {
+      let angle = Math.PI/2;
+      let pos = {
+        x: i * 10*RADIUS,
+        y: 0,
+      }
+      newRobot(pos, angle, new RobotRandom());
     }
-  }
-
-  setupGraphics(
-    PIXI,
-    pixiApp,
-    platformGraphics,
-    bodies,
-    bodyIDs,
-    setDisplayedData,
-  ) {
   }
 }
