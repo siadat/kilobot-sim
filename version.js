3fe3cd2a839f4b8d3257c2346efe2c5ea24a3011
Sina Siadat <siadat@gmail.com>
2019-12-20 01:41:37 +0330

    more readable colors

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 // Build stuff
-export const VERSION = '898107be09d8527abc721a754dc50bacfcec26da';
-export const BUILTAT = '2019-12-14-at-18-22-42';
+export const VERSION = '2c5cb9aec78c4545dab2f58dd465c40cc194bb4f';
+export const BUILTAT = '2019-12-20-at-01-41-24';
